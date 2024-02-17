c4cedc79914cebf710c5983329cfe8c57753f7e0
Vojtech Smejkal <smejkalv@gmail.com>
2024-02-17 12:15:56 +0100

    Improve Import/Export

--- src/service/ImportExportService.js
+++ src/service/ImportExportService.js
@@ -37,6 +37,9 @@ export async function exportWorkspaces() {
 	const workspaceIds = workspaceList.map((item) => item.workspaceId);
 	const workspaces = await chrome.storage.local.get(workspaceIds);
 
+	// Clean up windowIds
+	workspaceList.forEach((item) => item.windowId = null)
+
 	// Create
 	const data = JSON.stringify({ workspaceList, workspaces }, null, 2);
 	const blob = new Blob([data], { type: "application/json" });
@@ -44,12 +47,25 @@ export async function exportWorkspaces() {
 	// Download
 	const url = URL.createObjectURL(blob);
 	const link = document.createElement("a");
+	const datetime = getDateTimeString();
 	link.href = url;
-	link.download = "workspaces_backup.json";
+	link.download = `workspaces_${datetime}.json`;
 	document.body.appendChild(link);
 	link.click();
 
 	// Cleanup
 	document.body.removeChild(link);
 	URL.revokeObjectURL(url);
+}
+
+function getDateTimeString() {
+	const date = new Date();
+	const year = date.getFullYear().toString().padStart(4, "0");
+	const month = (date.getMonth() + 1).toString().padStart(2, "0");
+	const day = date.getDate().toString().padStart(2, "0");
+	const hours = date.getHours().toString().padStart(2, "0");
+	const minutes = date.getMinutes().toString().padStart(2, "0");
+	const seconds = date.getSeconds().toString().padStart(2, "0");
+
+	return `${year}${month}${day}${hours}${minutes}${seconds}`;
 }
